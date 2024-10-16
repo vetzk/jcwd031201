831787f60de26cd53f2c2a54e585c243d5e5959e
vetzk <letienspatisserie@gmail.com>
2024-10-16 13:33:29 +0700

    Update apiCall.ts

--- apps/web/src/helper/apiCall.ts
+++ apps/web/src/helper/apiCall.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiCall = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: process.env.NEXT_PUBLIC_BASE_API_URL,
 });
 
 if (typeof window !== 'undefined') {
